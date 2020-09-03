f83c4e7433aa8e07025f34b342f9ae234325a63f
Khubi Shah <shah.khubi@gmail.com>
2020-09-03 16:56:38 -0400

    Updates

---
+++ precache-manifest.34b312160f90b71a5219630c38612720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7388bd66fccdf0d57469426c0214afb",
+    "url": "/Monsters-Mockup/index.html"
+  },
+  {
+    "revision": "2bf108bd45f863ef9e10",
+    "url": "/Monsters-Mockup/static/css/main.afc2a0f1.chunk.css"
+  },
+  {
+    "revision": "947da912d02f49dbb754",
+    "url": "/Monsters-Mockup/static/js/2.ae963699.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Mockup/static/js/2.ae963699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bf108bd45f863ef9e10",
+    "url": "/Monsters-Mockup/static/js/main.b7da6897.chunk.js"
+  },
+  {
+    "revision": "6da735ace043dc4f0082",
+    "url": "/Monsters-Mockup/static/js/runtime-main.a91f29c3.js"
+  }
+]);
